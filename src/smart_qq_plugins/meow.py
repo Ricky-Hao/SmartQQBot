98e26e231efee2184164e7f694f39cae48fc70a6
Hao <a471558277@gmail.com>
2016-08-07 11:43:33 +0800

    Fix Bug

--- src/smart_qq_plugins/meow.py
+++ src/smart_qq_plugins/meow.py
@@ -1,4 +1,5 @@
 # coding:utf-8
+import random
 from smart_qq_bot.logger import logger
 from smart_qq_bot.signals import on_group_message
 import json
@@ -13,20 +14,14 @@ REPLY_SUFFIX = (
 @on_group_message(name='meow')
 def meow(msg,bot):
     group_code=json.load(file("./config/group_code.json"))
-    try:
-        if str(msg.group_code) in group_code.values():
-            if ur"喵喵喵" in msg.content:
-                try:
-                    logger.info('Meow to '+msg.group_code)
-                except:
-                    pass
-                bot.reply_msg(msg,"喵喵喵"+REPLY_SUFFIX)
-            elif ur"喵" in msg.content:
-                try:
-                    logger.info('Meow to '+msg.group_code)
-                except:
-                    pass
-                bot.reply_msg(msg,"喵"+REPLY_SUFFIX)
+    if str(msg.group_code) in group_code.values():
+        logger.debug("Ready to Meow")
+        if ur"喵喵喵" in msg.content:
+                logger.info('Meow to '+msg.group_code)
+            bot.reply_msg(msg,"喵喵喵"+random.choice(REPLY_SUFFIX))
+        elif ur"喵" in msg.content:
+                logger.info('Meow to '+msg.group_code)
+            bot.reply_msg(msg,"喵"+random.choice(REPLY_SUFFIX))
             return True
         return False
     except:
