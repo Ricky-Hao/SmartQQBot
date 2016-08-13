6add157de98b5b93cc2799c853f2a7c4d744145f
Hao <a471558277@gmail.com>
2016-08-13 23:42:52 +0800

    Fix

--- src/smart_qq_plugins/Tuling_bot.py
+++ src/smart_qq_plugins/Tuling_bot.py
@@ -60,12 +60,12 @@ def Tuling_robot(msg, bot):
                     bot.reply_msg(msg,response_json['url'])
                 elif opt=="菜谱信息":
                     content_list=random.choice(response_json['list'])
-                    bot.reply_msg(msg,content_list[i]['name']+'\n'+content_list[i]['detailurl'])
+                    bot.reply_msg(msg,content_list['name']+'\n'+content_list['detailurl'])
 
                 elif opt=="相关新闻":
                     content_list=random.choice(response_json['list'])
                     for i in range(0,3):
-                        bot.reply_msg(msg,content_list[i]['article']+'\n'+content_list[i]['detailurl'])
+                        bot.reply_msg(msg,content_list['article']+'\n'+content_list['detailurl'])
 
             elif utils.is_match('^亲，已帮你找到图片$',reply):
                 bot.reply_msg(msg,response_json['url'])
