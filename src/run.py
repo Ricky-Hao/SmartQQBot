976910c49e6ab8db56d69fb79db358bea46b6342
Hao <a471558277@gmail.com>
2016-10-02 19:10:29 +0800

    Fix Bug And Add XiaoIce Plugin

--- src/run.py
+++ src/run.py
@@ -20,8 +20,9 @@ if not os.path.exists(db):
     sql.execute('create table discu_data(id integer primary key autoincrement unique not null,fake_did varchar(20),discu_name varvhar(100));')
     sql.execute("create table plugins(id integer primary key autoincrement unique not null,account varchar(20),plugin_name varchar(100),account_type varchar(20));")
     with open("./config/plugin.json","r") as f:
-         plugins=json.load(f).get('plugin_on')
-         managers=json.load(f).get('manager')
+        t=f.read()
+    plugins=json.load(t).get('plugin_on')
+    managers=json.load(t).get('manager')
     for i in plugins:
         sql.execute("insert into plugins(account,plugin_name,account_type) values ('{0}','{1}','{2}');".format("00000",i,'group'))
     for i in managers:
---
+++ src/smart_qq_plugins/xiaoice.py
@@ -0,0 +1,56 @@
+# coding:utf-8
+import requests
+import json
+from smart_qq_bot.logger import logger
+from smart_qq_bot.signals import on_all_message
+import smart_qq_bot.sqlite as sql
+import smart_qq_bot.utils as utils
+import itchat
+import threading
+from itchat.content import *
+
+plugins_name="Xiaoice"
+HELP={
+    1:'XiaoIce: 微软小冰插件'
+}
+
+last_msg=""
+num=""
+last_bot=""
+
+
+class wechat(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        itchat.auto_login(hotReload=True,enableCmdQR=2)
+
+    @itchat.msg_register(TEXT,isMpChat=True)
+    def get_reply_from_xiaoice(msg):
+        global last_msg
+        global num
+        global last_bot
+        logger.info("[xiaoice] receive "+msg['Content'])
+        if(msg['FromUserName']==num):
+            last_bot.reply_msg(last_msg,msg['Content'])
+
+    def run(self):
+        itchat.run()
+
+    @on_all_message(name=plugins_name)
+    def xiaoice(msg,bot):
+        global last_msg
+        global num
+        global last_bot
+        (account,account_type)=utils.get_account_and_type(msg)
+        if utils.in_plugins(account,account_type,plugins_name):
+            nickname=sql.fetch_one('select nickname from Nickname where account="{0}" and account_type="{1}";'.format(account,account_type))[0]
+            if utils.is_match('^'+nickname+'\W(.+)',msg.content):
+                content=utils.is_match('^'+nickname+'\W(.+)',msg.content).group(1)
+                logger.info("[xiaoice] send "+content)
+                num=itchat.search_mps(name="小冰")[0]['UserName']
+                itchat.send_msg(msg=content, toUserName=num)
+                last_msg=msg
+                last_bot=bot
+
+t=wechat()
+t.start()
